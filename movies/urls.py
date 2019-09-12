7fa49c6f8e5db4982e8c2532849dbd844c4e0d69
sksho <pop.shofi@mail.com>
2019-09-12 11:46:57 +0600

    api added !

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,9 @@ from . import views
 # movies/
 # movies/1/details
 
+app_name = 'movies'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='movies_index'),
+    path('<int:movie_id>', views.detail, name='detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from django.http import  HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Movie
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    output = ",".join([m.title for m in movies])
+    return render(request, 'movies/index.html', {'movies': movies})
 
 
-
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
